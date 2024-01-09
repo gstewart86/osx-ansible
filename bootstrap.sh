0427c900317cb72e9b607516e059f243366b9be1
gstew <gstewart@es.net>
2024-01-09 12:07:27 -0800

    add homebrew to path automatically

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+# Dynamically set the user's home directory
+USER_HOME=$(eval echo ~$USER)
+
 # Check if Homebrew is installed, if not, install it
 if ! command -v brew &>/dev/null; then
     echo "Installing Homebrew..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    
+    # Dynamically update PATH in the user's shell profile
+    SHELL_PROFILE="${USER_HOME}/.zprofile" # Change this if using a different shell
+    echo "Adding Homebrew to PATH in ${SHELL_PROFILE}..."
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> "${SHELL_PROFILE}"
+    eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
 # Check if Ansible is installed, if not, install it using Homebrew
